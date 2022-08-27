a7122bddc3413095357fa88361c34df60ed1db4c
Riki Irawan <lightningthunder9999@gmail.com>
2022-08-27 01:00:30 +0000

    Upload New File

---
+++ service.sh
@@ -0,0 +1,133 @@
+MODPATH=${0%/*}
+API=`getprop ro.build.version.sdk`
+AML=/data/adb/modules/aml
+
+# debug
+exec 2>$MODPATH/debug.log
+set -x
+
+# property
+#resetprop ro.sys.oneplus.support 11.11.0
+resetprop ro.vendor.dolby.dax.version DAX3_3.5.6.11_r1
+#11resetprop ro.vendor.product.device.db OP_DEVICE
+#11resetprop ro.vendor.product.manufacturer.db OP_PHONE
+#10resetprop vendor.product.device OP_PHONE
+#10resetprop vendor.product.manufacturer OPD
+resetprop vendor.audio.dolby.ds2.enabled false
+resetprop vendor.audio.dolby.ds2.hardbypass false
+#resetprop vendor.audio.gef.debug.flags false
+#resetprop vendor.audio.gef.enable.traces false
+#resetprop vendor.dolby.dap.param.tee false
+#resetprop vendor.dolby.mi.metadata.log false
+
+# wait
+sleep 20
+
+# aml fix
+DIR=$AML/system/vendor/odm/etc
+if [ -d $DIR ] && [ ! -f $AML/disable ]; then
+  chcon -R u:object_r:vendor_configs_file:s0 $DIR
+fi
+
+# mount
+NAME="*audio*effects*.conf -o -name *audio*effects*.xml"
+#pNAME="*audio*effects*.conf -o -name *audio*effects*.xml -o -name *policy*.conf -o -name *policy*.xml"
+if [ ! -d $AML ] || [ -f $AML/disable ]; then
+  DIR=$MODPATH/system/vendor
+else
+  DIR=$AML/system/vendor
+fi
+FILE=`find $DIR/etc -maxdepth 1 -type f -name $NAME`
+if [ `realpath /odm/etc` == /odm/etc ] && [ "$FILE" ]; then
+  for i in $FILE; do
+    j="/odm$(echo $i | sed "s|$DIR||")"
+    if [ -f $j ]; then
+      umount $j
+      mount -o bind $i $j
+    fi
+  done
+fi
+if [ -d /my_product/etc ] && [ "$FILE" ]; then
+  for i in $FILE; do
+    j="/my_product$(echo $i | sed "s|$DIR||")"
+    if [ -f $j ]; then
+      umount $j
+      mount -o bind $i $j
+    fi
+  done
+fi
+
+# restart
+killall audioserver
+
+# function
+stop_service() {
+for NAMES in $NAME; do
+  if getprop | grep "init.svc.$NAMES\]: \[running"; then
+    stop $NAMES
+  fi
+done
+}
+run_service() {
+for FILES in $FILE; do
+  killall $FILES
+  $FILES &
+  PID=`pidof $FILES`
+done
+}
+
+# stop
+NAME="dms-hal-1-0 dms-hal-2-0 dms-v36-hal-2-0
+      vendor-ozoaudio-media-c2-hal-1-0"
+stop_service
+
+# run
+FILE="`realpath /vendor`/bin/hw/vendor.dolby.hardware.dms@2.0-service
+      `realpath /vendor`/bin/hw/vendor.ozoaudio.media.c2@1.0-service"
+run_service
+
+# restart
+killall com.dolby.daxservice
+VIBRATOR=`realpath /*/bin/hw/vendor.qti.hardware.vibrator.service*`
+[ "$VIBRATOR" ] && killall $VIBRATOR
+POWER=`realpath /*/bin/hw/vendor.mediatek.hardware.mtkpower@*-service`
+[ "$POWER" ] && killall $POWER
+killall android.hardware.usb@1.0-service
+killall android.hardware.sensors@2.0-service-mediatek
+killall android.hardware.light-service.mt6768
+CAMERA=`realpath /*/bin/hw/android.hardware.camera.provider@*-service_64`
+[ "$CAMERA" ] && killall $CAMERA
+
+# wait
+sleep 40
+
+# allow
+PKG=com.dolby.daxappui
+if pm list packages | grep $PKG ; then
+  if [ "$API" -ge 30 ]; then
+    appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+  fi
+fi
+
+# grant
+PKG=com.dolby.daxservice
+pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
+pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
+if [ "$API" -ge 30 ]; then
+  appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+fi
+
+# grant
+PKG=com.oneplus.sound.tuner
+if pm list packages | grep $PKG ; then
+  pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
+  pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
+  pm grant $PKG android.permission.READ_PHONE_STATE
+  pm grant $PKG android.permission.READ_CALL_LOG
+  if [ "$API" -ge 30 ]; then
+    appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+  fi
+fi
+
+
+
