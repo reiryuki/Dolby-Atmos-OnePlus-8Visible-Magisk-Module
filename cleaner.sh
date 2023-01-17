36bf61400c2629ae17aadd16d4c9b9a84a768361
Riki Irawan <lightningthunder9999@gmail.com>
2023-01-17 14:22:24 +0000

    Replace cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -4,10 +4,7 @@ for APPS in $APP; do
   rm -f `find /data/system/package_cache -type f -name *$APPS*`
   rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*.apk`
 done
-PKG="com.oneplus.sound.tuner
-     com.oneplus.config
-     com.dolby.daxappui
-     com.dolby.daxservice"
+PKG=`cat $MODPATH/package.txt`
 for PKGS in $PKG; do
   rm -rf /data/user/*/$PKGS/cache/*
 done
